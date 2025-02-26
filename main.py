3af083238ffb4251e4593dbca2a1a12c83a13424
Temirlan <temirlan.kazhigerey@gmail.com>
2025-02-26 16:58:25 +0500

    style: rewrited using isort

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
-from src.core.database import init_db
-from src.api.v1 import api_router
 from fastapi.middleware.cors import CORSMiddleware
 
+from src.api.v1 import api_router
+from src.core.database import init_db
+
+
 @asynccontextmanager
-async def lifespan(_:FastAPI):
+async def lifespan(_: FastAPI):
     await init_db()
-    yield 
+    yield
+
 
 def make_app():
     app = FastAPI(lifespan=lifespan)
-    app.include_router(api_router,)
+    app.include_router(
+        api_router,
+    )
     return app
+
 
 app = make_app()
 
@@ -26,4 +33,11 @@ app.add_middleware(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app",host="0.0.0.0",port=9001,reload=True,timeout_graceful_shutdown=360) 
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=9001,
+        reload=True,
+        timeout_graceful_shutdown=360,
+    )
